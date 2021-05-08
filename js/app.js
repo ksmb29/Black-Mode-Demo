d8159898758417cd4140f4c2df698555f0adcaa3
ksmb29 <ksmb295@gmail.com>
2021-05-07 22:27:25 -0500

    Social media dashboard with theme switcher

---
+++ js/app.js
@@ -0,0 +1,19 @@
+console.log()
+
+const checkbox = document.querySelector('#checkbox')
+if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+    console.log('true')
+    checkbox.setAttribute('checked', true)
+}
+
+
+checkbox.addEventListener('change', function(event){
+    
+    if(this.checked){
+        document.body.classList.add('is-dark-mode')
+        document.body.classList.remove('is-light-mode')
+    } else{
+        document.body.classList.remove('is-dark-mode')
+        document.body.classList.add('is-light-mode')
+    }
+})
